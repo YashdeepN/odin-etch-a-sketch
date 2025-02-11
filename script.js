ace23e15855d399efc39e79b7113fe0cb2259a11
YashdeepN <yashdeepneema@hotmail.com>
2025-02-11 11:37:35 -0600

    create 16*16 grid

---
+++ script.js
@@ -0,0 +1,23 @@
+//constants
+
+const GRID_SIZE = 16;
+
+//
+
+// to create grid
+const mainContainer = document.querySelector(".mainContainer");
+
+const gridRow = document.createElement("div");
+gridRow.classList.add("gridRow");
+gridRow.style.display = "flex";
+
+const singleBlock = document.createElement("div");
+singleBlock.classList.add("singleBlock");
+
+for (let i = 0; i < GRID_SIZE; ++i) {
+  gridRow.appendChild(singleBlock.cloneNode(true));
+}
+
+for (let i = 0; i < GRID_SIZE; ++i) {
+  mainContainer.appendChild(gridRow.cloneNode(true));
+}
